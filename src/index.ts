4a894913930de461a47e12b68ba7db3f339aab7d
pablo <mendez4a@gmail.com>
2021-03-15 10:45:41 +0100

    ignore tests

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import helmet from 'helmet';
-import express from 'express';
-import rateLimit from 'express-rate-limit';
+import helmet from "helmet";
+import express from "express";
+import rateLimit from "express-rate-limit";
 
-import { port } from './params/params';
-import { HttpError } from './utils/httpError';
-import { tcpPortsScan } from './tcpScan/tcpPortsScan';
-import { isValidIp, arePortsInRange, isInLimitNumberOfPorts } from './utils/requestAssertions';
-import { parseSanitizePortsReq } from './utils/parseSanitizeTcpPortsReq';
-import { PortsScan } from './types/types';
+import { port } from "./params/params";
+import { HttpError } from "./utils/httpError";
+import { tcpPortsScan } from "./tcpScan/tcpPortsScan";
+import { isValidIp, arePortsInRange, isInLimitNumberOfPorts } from "./utils/requestAssertions";
+import { parseSanitizePortsReq } from "./utils/parseSanitizeTcpPortsReq";
+import { PortsScan } from "./types/types";
 
 const app = express();
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 const limiter = rateLimit({
-	// default error message when too many requests: 'Too many requests, please try again later.'
-	// error code when too many requests: 429
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // limit each IP to 100 requests per windowMs
+  // default error message when too many requests: 'Too many requests, please try again later.'
+  // error code when too many requests: 429
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
 //  apply to all requests
@@ -25,49 +25,56 @@ app.use(helmet());
 
 // /173.249.41.199?tcpPorts=8092,1914&udpPorts=123,346,4325
 
-app.get('/:host', async (req, res) => {
-	try {
-		// Data declaration
-		let ports: { tcpPorts?: number[]; udpPorts?: number[] };
-		let portsScanResponse: PortsScan = {};
+app.get("/:host", async (req, res) => {
+  try {
+    // Data declaration
+    let ports: { tcpPorts?: number[]; udpPorts?: number[] };
+    let portsScanResponse: PortsScan = {};
 
-		// Data sanitization
-		ports = parseSanitizePortsReq(req);
-		const host = req.params.host;
-		if (!isValidIp(host)) throw new HttpError('Public ip not valid', 400);
+    // Data sanitization
+    ports = parseSanitizePortsReq(req);
+    const host = req.params.host;
+    if (!isValidIp(host)) throw new HttpError("Public ip not valid", 400);
 
-		// TCP ports scan and ports assertions
-		if (ports.tcpPorts) {
-			if (!arePortsInRange(ports.tcpPorts))
-				throw new HttpError('Port must be > 0 and < 65535', 400);
-			if (!isInLimitNumberOfPorts(ports.tcpPorts))
-				throw new HttpError('Too many ports introduced, no more than 20', 400);
+    // TCP ports scan and ports assertions
+    if (ports.tcpPorts) {
+      if (!arePortsInRange(ports.tcpPorts))
+        throw new HttpError("Port must be > 0 and < 65535", 400);
+      if (!isInLimitNumberOfPorts(ports.tcpPorts))
+        throw new HttpError("Too many ports introduced, no more than 20", 400);
 
-			try {
-				portsScanResponse.tcpPorts = await tcpPortsScan({ ports: ports.tcpPorts, host });
-			} catch (e) {
-				throw new HttpError(`Error scanning tcp ports. Error: ${e}`, 500);
-			}
-		}
+      try {
+        portsScanResponse.tcpPorts = await tcpPortsScan({ ports: ports.tcpPorts, host });
+      } catch (e) {
+        throw new HttpError(`Error scanning tcp ports. Error: ${e}`, 500);
+      }
+    }
 
-		// UDP ports scan and data assertions
-		/* if (ports.udpPorts) {
+    // UDP ports scan and data assertions
+    /* if (ports.udpPorts) {
 			return;
 		} */
 
-		res.json(portsScanResponse);
-	} catch (e) {
-		if (res.headersSent) {
-			res.end();
-		}
-		if (e instanceof HttpError) {
-			res.status(e.code).send(e.message);
-		} else {
-			res.status(500).send(e.message);
-		}
-	}
+    res.json(portsScanResponse);
+  } catch (e) {
+    if (res.headersSent) {
+      res.end();
+    }
+    if (e instanceof HttpError) {
+      res.status(e.code).send(e.message);
+    } else {
+      res.status(500).send(e.message);
+    }
+  }
 });
 
-app.listen(port, () => {
-	console.log(`Scan ports app listening at http://localhost:${port}`);
+const server = app.listen(port, () => {
+  console.log(`Scan ports app listening at http://localhost:${port}`);
 });
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+function shutdown() {
+  console.log("Closing server...");
+  server.close((err) => console.log(err || "Bye"));
+}
